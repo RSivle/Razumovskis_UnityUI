d1be3ec142c85b914013a97940b34e42a99eb859
RSivle <geto@inbox.lv>
2026-02-17 13:27:07 +0200

    izveidota izvēlne ar fona mūziku

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [Header("Audio")]
+    public AudioSource buttonClickSound;
+
+    
+    public void StartGame()
+    {
+        PlayButtonClick();
+        
+        SceneManager.LoadScene(1);
+    }
+
+    
+    public void QuitGame()
+    {
+        PlayButtonClick();
+        Application.Quit();
+        
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+
+    private void PlayButtonClick()
+    {
+        if (buttonClickSound != null)
+            buttonClickSound.Play();
+    }
+}
